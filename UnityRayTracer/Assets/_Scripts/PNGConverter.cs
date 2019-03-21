9c70040b6c953ac0c2f4ca1cc7e3c3026a4f46fe
dorukcoskun <1dorukcoskun@gmail.com>
2019-03-21 16:00:50 +0300

    NeedFix Transparency bug in mirror reflections

---
+++ UnityRayTracer/Assets/_Scripts/PNGConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.IO;
+
+public class PNGConverter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return TakeScreenShot();
+    }
+
+    IEnumerator TakeScreenShot() 
+    {
+        yield return new WaitForEndOfFrame();
+
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+
+        byte[] bytes = tex.EncodeToPNG();
+        Destroy(tex);
+
+        Debug.Log(Application.dataPath + "/Resources/ScreenShot.png");
+        File.WriteAllBytes(Application.dataPath + "/Resources/ScreenShot.png", bytes);
+    }
+}
